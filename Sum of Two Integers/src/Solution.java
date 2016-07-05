8c1fe0c24d04e31e101909312910a08f6126425c
hzy <595125868@qq.com>
2016-07-05 15:26:34 +0800

    integer 相加

---
+++ Sum of Two Integers/src/Solution.java
@@ -0,0 +1,19 @@
+/**
+ * Created by hzy-pc on 2016/7/5.
+ */
+public class Solution {
+    /**
+     exclusive or (^) handles these cases: 1+0 and 0+1
+     AND (&) handles this case: 1+1, where carry occurs, in this case,
+     we'll have to shift carry to the left, why? Think about this example: 001 + 101 = 110 (binary format),
+     the least significant digits of the two operands are both '1', thus trigger a carry = 1,
+     with this carry, their least significant digits: 1+1 = 0,thus we need to shift the carry to the
+     left by 1 bit in order to get their correct sum: 2
+     */
+    public int getSum(int a, int b) {
+        if(b == 0) return a;
+        int carry = (a & b) << 1;
+        int sum = a ^ b;
+        return getSum(sum, carry);
+    }
+}
